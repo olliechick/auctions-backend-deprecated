b9eee28cee4cf527022ab168e16a398d00f6dd72
Ollie Chick <och26@uclive.ac.nz>
2018-03-11 17:34:08 +1300

    create rudimentary method for creating an auction. currently requires the creation date and userid to be passed in, and doesn't allow primaryphoto_URI

--- app/controllers/auction.server.controller.js
+++ app/controllers/auction.server.controller.js
@@ -7,7 +7,33 @@ exports.list = function(req, res){
 };
 
 exports.create = function(req, res){
-    return null;
+    let auction_data = {
+        "title": req.body.title,
+        "categoryid": req.body.categoryid,
+        "description": req.body.description,
+        "reserveprice": req.body.reserveprice,
+        "startingprice": req.body.startingprice,
+        "creationdate": req.body.creationdate,
+        "startingdate": req.body.startingdate,
+        "endingdate": req.body.endingdate,
+        "userid": req.body.userid
+    };
+
+    let values = [
+        [auction_data['title'].toString()],
+        [auction_data['categoryid'].toString()],
+        [auction_data['description'].toString()],
+        [auction_data['reserveprice'].toString()],
+        [auction_data['startingprice'].toString()],
+        [auction_data['creationdate'].toString()],
+        [auction_data['startingdate'].toString()],
+        [auction_data['endingdate'].toString()],
+        [auction_data['userid'].toString()]
+    ];
+
+    Auction.insert(values, function(result){
+        res.json(result);
+    });
 };
 
 exports.view = function(req, res){
--- app/models/auction.server.model.js
+++ app/models/auction.server.model.js
@@ -1,20 +1,28 @@
 const db = require('../../config/db');
 
-exports.getOne = function(){
+exports.getOne = function () {
     return null;
 };
 
-exports.getAll = function(done){
-    db.get_pool().query("SELECT * FROM auction", function(err, rows) {
+exports.getAll = function (done) {
+    db.get_pool().query("SELECT * FROM auction", function (err, rows) {
         if (err) return done({"ERROR": "Error selecting"});
         return done(rows);
     });
 };
 
-exports.insert = function(){
-    return null;
+exports.insert = function (values, done) {
+    let queryString = "INSERT INTO auction(auction_title, auction_categoryid, auction_description, " +
+        "auction_reserveprice, auction_startingprice, auction_creationdate, auction_startingdate, auction_endingdate, " +
+        "auction_userid) VALUES (?" + ", ?".repeat(9-1) + ")";
+    db.get_pool().query(queryString, values, function (err, result) {
+
+        if (err) return done(err);
+
+        done(result);
+    });
 };
 
-exports.alter = function(){
+exports.alter = function () {
     return null;
 };
