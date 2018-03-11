970aaf93773d202e4aaeb95b43e0e6e4341b5171
Ollie Chick <och26@uclive.ac.nz>
2018-03-11 17:02:18 +1300

    change to port 4941

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ db.connect(function (err) {
         console.log("Unable to connect to MySQL.");
         process.exit(1);
     } else {
-        app.listen(3000, function () {
-            console.log("Listening on port: " + 3000);
+        app.listen(4941, function () {
+            console.log("Listening on port: " + 4941);
         });
     }
 });
